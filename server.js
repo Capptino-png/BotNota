90ca95a2f3d94e1730c661f865941f0f676e002a
Capp <capptino@outlook.com>
2026-01-29 09:24:38 -0300

    add phone number

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Bot Online');
-}).listen(PORT);
+  res.end('Bot WhatsApp Online');
+}).listen(PORT, () => {
+  console.log('Servidor HTTP rodando na porta', PORT);
+});
 
-require('./index.js');
+// Forçar uso de código com seu número
+process.argv = ['node', 'connect.js', 'sim'];
+process.env.PHONE_NUMBER = '5511948808735';  // Ex: 5511999999999
+
+require('./connect.js');
